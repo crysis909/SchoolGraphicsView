5109552bde85442c0d6e54cdf4b378443dbd9713
listenprojekt <andreas.heinbach@htl-ottakring.ac.at>
2018-04-19 17:54:41 +0200

    constructor redefined

--- q4xbkifgraphicsview.cpp
+++ q4xbkifgraphicsview.cpp
@@ -1,6 +1,7 @@
 #include "q4xbkifgraphicsview.h"
 
-Q4xBKIFGraphicsView::Q4xBKIFGraphicsView()
+Q4xBKIFGraphicsView::Q4xBKIFGraphicsView(QWidget *parent, int x, int y, int w, int h)
+  :QGraphicsView(parent)
 {
 
 }
