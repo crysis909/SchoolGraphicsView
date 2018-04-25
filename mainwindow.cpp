3380fdf1e0fba1dadc13a065a2d4a201ab884272
listenprojekt <andreas.heinbach@htl-ottakring.ac.at>
2018-04-25 20:02:02 +0200

    slot implemented labels added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,9 +8,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     myView= new Q4xBKIFGraphicsView(this,10,10,450,450);
     myView->show();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::exitposition(int x, int y)
+{
+    ui->label->setText(QString::number(x));
+    ui->label_2->setText(QString::number(y));
 }
