51e799656ba0a7f2a8487ee8a2d85a2f98d0ec26
Markus <crysis909@users.noreply.github.com>
2018-04-28 00:41:00 +0200

    Adjusting the layout and adding comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,33 +6,37 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     //Init
     myView = new Q4xBKIFGraphicsView(this,10,10,450,450);
     layout = new QGridLayout(this);
     widget = new QWidget(this);
 
     //Layout
-    layout->addWidget(myView,1,1,2,2);
-    layout->addWidget(ui->graphicsView,1,3,2,1);
-    layout->addWidget(ui->label_x,3,4,2,1);
-    layout->addWidget(ui->label_y,3,5,2,1);
+    layout->addWidget(myView,0,0,Qt::AlignTop);
+    layout->addWidget(ui->graphicsView,0,1,Qt::AlignTop);
+    layout->addWidget(ui->label_x,0,1,Qt::AlignLeft);
+    layout->addWidget(ui->label_y,0,1,Qt::AlignJustify);
 
+    //Set default Text
     ui->label_x->setText("X: 0");
     ui->label_y->setText("Y: 0");
 
-    myView->show();
-
+    //Connects
     connect(myView,SIGNAL(getposition(int,int)),this,SLOT(exitposition(int,int)));
 
+    myView->show();
+
+    //Set Widget and Layout to Mainwindow
     widget->setLayout(layout);
     setCentralWidget(widget);
 }
 
 MainWindow::~MainWindow()
 {
+    delete myView;
     delete layout;
     delete widget;
-    delete myView;
     delete ui;
 }
 
